270cc282d45e18be930d7bee9000b3d8cb615aaf
Clem Hepburn <clemhepburn@gmail.com>
2021-08-16 08:56:38 -0700

    create Channel

--- src/components/characters/Channel.jsx
+++ src/components/characters/Channel.jsx
@@ -2,3 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Channel = ({ title }) => (
+  <div>
+    <span>{title}</span>
+  </div>
+);
+
+Channel.propTypes = {
+  title: PropTypes.string.isRequired
+};
+
+export default Channel;
